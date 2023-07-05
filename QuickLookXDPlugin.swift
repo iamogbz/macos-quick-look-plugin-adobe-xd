a59c8ad5d520c59f2729d461f772b843f9f464a4
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2023-07-05 11:24:44 -0400

    chore: code formatting

--- QuickLookXDPlugin.swift
+++ QuickLookXDPlugin.swift
@@ -11,92 +11,98 @@ import SSZipArchive
 
 @available(macOS 12.0, *)
 public class QuickLookXDPlugin: NSViewController, QLPreviewingController {
-    let imageFileNamePreview = "preview.png"
-    let imageFileNameThumbnail = "thumbnail.png"
+  struct Constants {
+    static let imageFileNamePreview = "preview.png"
+    static let imageFileNameThumbnail = "thumbnail.png"
+  }
 
-    public override var nibName: NSNib.Name? {
-        return NSNib.Name("QuickLookXDPlugin")
+  public override var nibName: NSNib.Name? {
+    return NSNib.Name("QuickLookXDPlugin")
+  }
+
+  public override func loadView() {
+    super.loadView()
+    // Do any additional setup after loading the view.
+  }
+
+  func extractImageOfFile(at url: URL, imageFileName: String, completionHandler handler: @escaping (String?, Error?) -> Void) {
+    let xdFilePath = url.absoluteString
+
+    if !url.isFileURL {
+      let errorMsg = "Provided URL is not a file path: \(xdFilePath)"
+      handler(nil, errorMsg as? Error)
     }
 
-    public override func loadView() {
-        super.loadView()
-        // Do any additional setup after loading the view.
+    let tempDirectory = NSTemporaryDirectory()
+
+    NSLog("extract image of file: \(xdFilePath) -> \(tempDirectory)")
+
+    SSZipArchive.unzipFile(atPath: xdFilePath, toDestination: tempDirectory)
+
+    let imageFilePath = "\(tempDirectory)/\(imageFileName)"
+
+    handler(imageFilePath, nil)
+  }
+
+  public func preparePreviewOfFile(at url: URL, completionHandler handler: @escaping (Error?) -> Void) {
+    NSLog("prepare preview: \(url)")
+    extractImageOfFile(at: url, imageFileName: Constants.imageFileNamePreview) { _, error in
+      handler(error)
     }
+  }
 
-    func extractImageOfFile(at url: URL, imageFileName: String, completionHandler handler: @escaping (String?, Error?) -> Void) {
-        let xdFilePath = url.absoluteString
+  /*
+   Use a QLPreviewProvider to provide data-based previews.
 
-        if !url.isFileURL {
-            let errorMsg = "Provided URL is not a file path: \(xdFilePath)"
-            handler(nil, errorMsg as? Error)
-        }
+   To set up your extension as a data-based preview extension:
 
-        let tempDirectory = NSTemporaryDirectory()
+   - Modify the extension's Info.plist by setting
+     <key>QLIsDataBasedPreview</key>
+     <true/>
 
-        NSLog("extract image of file: \(xdFilePath) -> \(tempDirectory)")
+   - Add the supported content types to QLSupportedContentTypes array in the extension's Info.plist.
 
-        SSZipArchive.unzipFile(atPath: xdFilePath, toDestination: tempDirectory)
+   - Change the NSExtensionPrincipalClass to this class.
+     e.g.
+     <key>NSExtensionPrincipalClass</key>
+     <string>${PRODUCT_BUNDLE_IDENTIFIER}.QuickLookXDPlugin</string>
 
-        let imageFilePath = "\(tempDirectory)/\(imageFileName)"
-
-        handler(imageFilePath, nil)
+   - Implement providePreview(for:)
+   - Implement provideThumbnail(for:)
+   */
+  public func providePreview(for request: QLFilePreviewRequest, completionHandler: @escaping (QLPreviewReply?, Error?) -> Void) {
+    NSLog("provide preview: \(request.fileURL)")
+    let imageFileName = Constants.imageFileNamePreview
+    extractImageOfFile(at: request.fileURL, imageFileName: imageFileName) { imageFilePath, error in
+      if (error != nil) {
+        completionHandler(nil, error)
+      } else if (imageFilePath == nil) {
+        let errorMsg = "Failed to extract from XD file: \(imageFileName)"
+        completionHandler(nil, errorMsg as? Error)
+      } else {
+        let reply = QLPreviewReply(fileURL: URL(string: imageFilePath!)!)
+        completionHandler(reply, nil)
+      }
     }
+  }
 
-    public func preparePreviewOfFile(at url: URL, completionHandler handler: @escaping (Error?) -> Void) {
-        NSLog("prepare preview: \(url)")
-        extractImageOfFile(at: url, imageFileName: self.imageFileNamePreview) { _, error in
-           handler(error)
-        }
+  public func provideThumbnail(for request: QLFileThumbnailRequest, completionHandler: @escaping (QLThumbnailReply?, Error?) -> Void) {
+    NSLog("provide thumbnail: \(request.fileURL)")
+    let imageFileName = Constants.imageFileNameThumbnail
+    extractImageOfFile(at: request.fileURL, imageFileName: imageFileName) { imageFilePath, error in
+      if (error != nil) {
+        completionHandler(nil, error)
+      } else if (imageFilePath == nil) {
+        let errorMsg = "Failed to extract from XD file: \(imageFileName)"
+        completionHandler(nil, errorMsg as? Error)
+      } else {
+        let reply = QLThumbnailReply(imageFileURL: URL(string: imageFilePath!)!)
+        completionHandler(reply, nil)
+      }
     }
-
-    /*
-     Use a QLPreviewProvider to provide data-based previews.
-
-     To set up your extension as a data-based preview extension:
-
-     - Modify the extension's Info.plist by setting
-       <key>QLIsDataBasedPreview</key>
-       <true/>
-
-     - Add the supported content types to QLSupportedContentTypes array in the extension's Info.plist.
-
-     - Change the NSExtensionPrincipalClass to this class.
-       e.g.
-       <key>NSExtensionPrincipalClass</key>
-       <string>${PRODUCT_BUNDLE_IDENTIFIER}.QuickLookXDPlugin</string>
-
-     - Implement providePreview(for:)
-     - Implement provideThumbnail(for:)
-     */
-    public func providePreview(for request: QLFilePreviewRequest, completionHandler: @escaping (QLPreviewReply?, Error?) -> Void) {
-        NSLog("provide preview: \(request.fileURL)")
-        let imageFileName = self.imageFileNamePreview
-        extractImageOfFile(at: request.fileURL, imageFileName: imageFileName) { imageFilePath, error in
-            if (error != nil) {
-                completionHandler(nil, error)
-            } else if (imageFilePath == nil) {
-                let errorMsg = "Failed to extract from XD file: \(imageFileName)"
-                completionHandler(nil, errorMsg as? Error)
-            } else {
-                let reply = QLPreviewReply(fileURL: URL(string: imageFilePath!)!)
-                completionHandler(reply, nil)
-            }
-        }
-    }
-
-    public func provideThumbnail(for request: QLFileThumbnailRequest, completionHandler: @escaping (QLThumbnailReply?, Error?) -> Void) {
-        NSLog("provide thumbnail: \(request.fileURL)")
-        let imageFileName = self.imageFileNameThumbnail
-        extractImageOfFile(at: request.fileURL, imageFileName: imageFileName) { imageFilePath, error in
-            if (error != nil) {
-                completionHandler(nil, error)
-            } else if (imageFilePath == nil) {
-                let errorMsg = "Failed to extract from XD file: \(imageFileName)"
-                completionHandler(nil, errorMsg as? Error)
-            } else {
-                let reply = QLThumbnailReply(imageFileURL: URL(string: imageFilePath!)!)
-                completionHandler(reply, nil)
-            }
-        }
-    }
+  }
+  
+  static func generatorFactory(args: Any...) {
+    NSLog("QuickLookXD.generatorFactory")
+  }
 }
